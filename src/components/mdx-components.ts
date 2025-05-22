bb12b43aa539069d01163f2a76226c359ddd54ab
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-05-22 17:50:47 -0500

    Alert Component (#6)

--- src/components/mdx-components.ts
+++ src/components/mdx-components.ts
@@ -1,5 +1,15 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
+import { BiErrorAlt } from "react-icons/bi";
+import { FiCheck, FiDownload, FiInfo } from "react-icons/fi";
+import { IoWarningOutline } from "react-icons/io5";
 
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertRoot,
+  AlertTitle,
+} from "@/registry/thornberry/components/alert";
 import { Button } from "@/registry/thornberry/components/button";
 import { Input } from "@/registry/thornberry/components/input";
 
@@ -8,8 +18,18 @@ import type { MDXComponents } from "mdx/types";
 export const getMDXComponents = (components?: MDXComponents): MDXComponents => {
   return {
     ...defaultMdxComponents,
+    Alert,
+    AlertDescription,
+    AlertIcon,
+    AlertRoot,
+    AlertTitle,
+    BiErrorAlt,
     Button,
+    FiCheck,
+    FiDownload,
+    FiInfo,
     Input,
+    IoWarningOutline,
     ...components,
   };
 };
---
+++ src/registry/thornberry/components/alert.tsx
@@ -0,0 +1,111 @@
+import { ark } from "@ark-ui/react";
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+import type { VariantProps } from "class-variance-authority";
+import type { ComponentProps, ReactNode } from "react";
+
+const alertVariants = cva(
+  "relative w-full rounded-lg border px-4 py-5 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
+  {
+    variants: {
+      variant: {
+        default: "bg-background text-foreground",
+        error:
+          "bg-destructive/5 border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+        info: "bg-blue-500/5 border-blue-500/50 text-blue-500 dark:border-blue-500 [&>svg]:text-blue-500",
+        warning:
+          "bg-yellow-500/5 border-yellow-500/50 text-yellow-500 dark:border-yellow-500 [&>svg]:text-yellow-500",
+        success:
+          "bg-green-500/5 border-green-500/50 text-green-500 dark:border-green-500 [&>svg]:text-green-500",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+const AlertRoot = ({
+  className,
+  variant,
+  ...rest
+}: ComponentProps<typeof ark.div> & VariantProps<typeof alertVariants>) => (
+  <ark.div
+    role="alert"
+    className={cn(alertVariants({ variant }), className)}
+    {...rest}
+  />
+);
+
+const AlertIcon = ({ className, ...rest }: ComponentProps<typeof ark.svg>) => (
+  <ark.svg
+    className={cn("text-foreground shrink-0 size-5", className)}
+    asChild
+    {...rest}
+  />
+);
+
+const AlertTitle = ({ className, ...rest }: ComponentProps<typeof ark.div>) => (
+  <ark.div
+    className={cn(
+      "-mt-1 mb-1 font-bold leading-none tracking-tight text-lg",
+      className,
+    )}
+    {...rest}
+  />
+);
+
+const AlertDescription = ({
+  className,
+  ...rest
+}: ComponentProps<typeof ark.div>) => (
+  <ark.div
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...rest}
+  />
+);
+
+interface AlertProps
+  extends ComponentProps<typeof ark.div>,
+    VariantProps<typeof alertVariants> {
+  title?: string;
+  description?: string;
+  icon?: ReactNode;
+  titleProps?: ComponentProps<typeof ark.div>;
+  descriptionProps?: ComponentProps<typeof ark.div>;
+  iconProps?: ComponentProps<typeof ark.svg>;
+}
+
+const Alert = ({
+  className,
+  variant,
+  title,
+  description,
+  icon,
+  titleProps,
+  descriptionProps,
+  iconProps,
+  children,
+  ...rest
+}: AlertProps) => (
+  <AlertRoot className={cn(alertVariants({ variant }), className)} {...rest}>
+    {icon && <AlertIcon {...iconProps}>{icon}</AlertIcon>}
+    {title && <AlertTitle {...titleProps}>{title}</AlertTitle>}
+    {description && (
+      <AlertDescription {...descriptionProps}>{description}</AlertDescription>
+    )}
+    {children}
+  </AlertRoot>
+);
+
+export {
+  Alert,
+  AlertRoot,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  alertVariants,
+  type AlertProps,
+};
