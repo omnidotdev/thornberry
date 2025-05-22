7ac8f459fe91c07673fb5f3ce21b3d0ff5304ebd
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-05-22 14:30:56 -0500

    refactor: update biome config

--- .source/index.ts
+++ .source/index.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck -- skip type checking
-import * as docs_2 from "../src/content/docs/input.mdx?collection=docs&hash=1747941908924"
-import * as docs_1 from "../src/content/docs/index.mdx?collection=docs&hash=1747941908924"
-import * as docs_0 from "../src/content/docs/button.mdx?collection=docs&hash=1747941908924"
+import * as docs_2 from "../src/content/docs/input.mdx?collection=docs&hash=1747937714639"
+import * as docs_1 from "../src/content/docs/index.mdx?collection=docs&hash=1747937714639"
+import * as docs_0 from "../src/content/docs/button.mdx?collection=docs&hash=1747937714639"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"button.mdx","absolutePath":"/Users/hawk/dev/omni/thornberry/src/content/docs/button.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","absolutePath":"/Users/hawk/dev/omni/thornberry/src/content/docs/index.mdx"}, data: docs_1 }, { info: {"path":"input.mdx","absolutePath":"/Users/hawk/dev/omni/thornberry/src/content/docs/input.mdx"}, data: docs_2 }], [{"info":{"path":"meta.json","absolutePath":"/Users/hawk/dev/omni/thornberry/src/content/docs/meta.json"},"data":{"pages":["---Introduction---","index","---Components---","button","input"],"root":true}}])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"button.mdx","absolutePath":"/Users/hobbescodes/Desktop/hobbescodes/omni/in-house/thornberry/src/content/docs/button.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","absolutePath":"/Users/hobbescodes/Desktop/hobbescodes/omni/in-house/thornberry/src/content/docs/index.mdx"}, data: docs_1 }, { info: {"path":"input.mdx","absolutePath":"/Users/hobbescodes/Desktop/hobbescodes/omni/in-house/thornberry/src/content/docs/input.mdx"}, data: docs_2 }], [{"info":{"path":"meta.json","absolutePath":"/Users/hobbescodes/Desktop/hobbescodes/omni/in-house/thornberry/src/content/docs/meta.json"},"data":{"pages":["---Introduction---","index","---Components---","button","input"],"root":true}}])
