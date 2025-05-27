bb9d0885922304eadb8a5444e8c097629c73168c
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-05-27 18:54:16 -0500

    refactor: add API reference for each component

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,11 +1,18 @@
+import { createGenerator } from "fumadocs-typescript";
+import { AutoTypeTable } from "fumadocs-typescript/ui";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 
 import type { MDXComponents } from "mdx/types";
 
+const generator = createGenerator();
+
 export const getMDXComponents = (components?: MDXComponents): MDXComponents => {
   return {
     ...defaultMdxComponents,
+    AutoTypeTable: (props) => (
+      <AutoTypeTable {...props} generator={generator} />
+    ),
     ...TabsComponents,
     ...components,
   };
