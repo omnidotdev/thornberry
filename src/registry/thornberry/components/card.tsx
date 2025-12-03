3b73af31b442f1943d07841e533e1ab4a28a333e
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-12-03 14:50:48 -0600

    chore: remove pre-built card components and update mdx file

--- src/registry/thornberry/components/card.tsx
+++ src/registry/thornberry/components/card.tsx
@@ -2,7 +2,7 @@ import { ark } from "@ark-ui/react";
 
 import { cn } from "@/lib/utils";
 
-import type { ComponentProps, ReactNode } from "react";
+import type { ComponentProps } from "react";
 
 const CardRoot = ({ className, ...rest }: ComponentProps<typeof ark.div>) => (
   <ark.div
@@ -46,67 +46,11 @@ const CardFooter = ({ className, ...rest }: ComponentProps<typeof ark.div>) => (
   <ark.div className={cn("flex items-center p-6 pt-0", className)} {...rest} />
 );
 
-interface CardProps extends ComponentProps<typeof CardRoot> {
-  title?: string;
-  description?: string;
-  footer?: ReactNode;
-  /**
-   * @remarks `ComponentProps<typeof CardHeader>`
-   */
-  headerProps?: ComponentProps<typeof CardHeader>;
-  /**
-   * @remarks `ComponentProps<typeof CardTitle>`
-   */
-  titleProps?: ComponentProps<typeof CardTitle>;
-  /**
-   * @remarks `ComponentProps<typeof CardDescription>`
-   */
-  descriptionProps?: ComponentProps<typeof CardDescription>;
-  /**
-   * @remarks `ComponentProps<typeof CardContent>`
-   */
-  contentProps?: ComponentProps<typeof CardContent>;
-  /**
-   * @remarks `ComponentProps<typeof CardFooter>`
-   */
-  footerProps?: ComponentProps<typeof CardFooter>;
-}
-
-const Card = ({
-  title,
-  description,
-  footer,
-  headerProps,
-  titleProps,
-  descriptionProps,
-  contentProps,
-  footerProps,
-  children,
-  ...rest
-}: CardProps) => (
-  <CardRoot {...rest}>
-    {(title || description) && (
-      <CardHeader {...headerProps}>
-        {title && <CardTitle {...titleProps}>{title}</CardTitle>}
-        {description && (
-          <CardDescription {...descriptionProps}>{description}</CardDescription>
-        )}
-      </CardHeader>
-    )}
-
-    <CardContent {...contentProps}>{children}</CardContent>
-
-    {footer && <CardFooter {...footerProps}>{footer}</CardFooter>}
-  </CardRoot>
-);
-
 export {
-  Card,
   CardRoot,
   CardHeader,
   CardFooter,
   CardTitle,
   CardDescription,
   CardContent,
-  type CardProps,
 };
