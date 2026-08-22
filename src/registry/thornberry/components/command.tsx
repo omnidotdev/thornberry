b32abda40e19f6d5e760f55b48bbff0b731eb20c
Brian Cooper <brian@brian-cooper.com>
2026-08-22 11:43:20 -0700

    feat(command): wrap keyboard navigation past the list ends
    Enable cmdk's `loop` on the command palette dialog so ArrowDown on the
    last item moves to the first and ArrowUp on the first moves to the last.

--- src/registry/thornberry/components/command.tsx
+++ src/registry/thornberry/components/command.tsx
@@ -55,7 +55,10 @@ const CommandDialog = ({
           <DialogDescription className="sr-only">
             {description}
           </DialogDescription>
-          <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:size-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:size-5">
+          <Command
+            loop
+            className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:size-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:size-5"
+          >
             {children}
           </Command>
         </DialogContent>
